284cdefe281e3969ad29ffb16342041bc8d659c0
AJ Speller <ajspeller@gmail.com>
2019-04-21 17:22:39 -0400

    Setup socket.io on the server and client

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const debug = require('debug')('app');
 const chalk = require('chalk');
 const helmet = require('helmet');
 const morgan = require('morgan');
+const socket = require('socket.io');
 
 const app = express();
 
@@ -17,3 +18,10 @@ const server = app.listen(PORT, () => {
 // -- middleware
 // setup static files
 app.use(express.static('public'));
+
+// setup socket
+const io = socket(server);
+
+io.on('connection', (socket) => {
+  debug(`Socket connection successful! ${chalk.green.inverse(socket.id)}`);
+});
